7f0706703b98fc5a12088e6b05bba19f9259e354
victorcellos <victor.cellos2@gmail.com>
2023-06-25 09:59:38 -0300

    Atualização do site;

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 </head>
 
 <body>
-
     <nav class="navbar navbar-default custom-navbar">
         <div class="container">
             <div class="navbar-header">
@@ -20,7 +19,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#"><img src="logo.png" alt="Logo"></a>
+                <a class="navbar-brand" href="#"><img src="img/logo.png" class="logo" alt="Logo"></a>
             </div>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="nav navbar-nav navbar-right">
@@ -33,35 +32,34 @@
         </div>
     </nav>
 
-    <div id="carouselFade" class="carousel slide carousel-fade" data-ride="carousel">
-        <div class="carousel-inner" role="listbox">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+        </ol>
+
+        <div class="carousel-inner">
             <div class="item active">
+                <img src="img/banner-cash.png" alt="Image 1">
                 <div class="carousel-caption">
-                    <h3>1ere Image<h3>
-                            <p>Voici la (taataataaaa) première image ;p</p>
+
                 </div>
             </div>
+
             <div class="item">
+                <img src="img/banner-fret.png" alt="Image 2">
                 <div class="carousel-caption">
-                    <h3>Second slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </div>
-            </div>
-            <div class="item">
-                <div class="carousel-caption">
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+
                 </div>
             </div>
         </div>
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carouselFade" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#carouselFade" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
             <span class="sr-only">Next</span>
         </a>
     </div>
