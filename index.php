1a976b4fae0c900e7ee88064ab3a3c68a5f445ae
victorcellos <victor.cellos2@gmail.com>
2023-06-25 11:17:05 -0300

    Atualização;

--- index.php
+++ index.php
@@ -5,68 +5,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu de Navegação</title>
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-default custom-navbar">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarNav">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#"><img src="img/logo.png" class="logo" alt="Logo"></a>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#">Nexxus Inc</a></li>
-                    <li><a href="#">Produtos</a></li>
-                    <li><a href="#">Diferenciais</a></li>
-                    <li><a href="#">Área do Cliente</a></li>
-                </ul>
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom navbar-dark">
+        <div class="navbar-brand">
+            <img src="img/logo.png" class="logo" alt="Logo">
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Nexxus Inc</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Assinar Planos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Diferenciais</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Área do Cliente</a>
+                </li>
+            </ul>
         </div>
     </nav>
 
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-        </ol>
-
-        <div class="carousel-inner">
-            <div class="item active">
-                <img src="img/banner-cash.png" alt="Image 1">
-                <div class="carousel-caption">
-
+    <div class="carousel-custom">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" style="width: 80%; margin: auto;">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="img/banner-cash.png" alt="Primeira imagem">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/banner-fret.png" alt="Segunda imagem">
                 </div>
             </div>
-
-            <div class="item">
-                <img src="img/banner-fret.png" alt="Image 2">
-                <div class="carousel-caption">
-
-                </div>
-            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Próximo</span>
+            </a>
         </div>
-
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-            <span class="sr-only">Next</span>
-        </a>
     </div>
 
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <!-- Scripts do Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
